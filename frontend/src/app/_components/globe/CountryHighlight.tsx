61ce251f649aea8dfe313c687079629593ac8ef3
Barru Adi <barru.adi@gmail.com>
2025-05-06 14:11:55 +0700

    feat: globe v1

---
+++ frontend/src/app/_components/globe/CountryHighlight.tsx
@@ -0,0 +1,15 @@
+import { CountryData } from "@/app/_types/types"
+
+const CountryHighlight = ({
+    countryData,
+    onCountryClick
+}: {
+    countryData?: CountryData[], 
+    onCountryClick?: (country: CountryData) => void 
+}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
