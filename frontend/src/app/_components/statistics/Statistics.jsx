50b0852d74b5af9230d2611eb3410a11f5d6e65e
brihadianSTEI <19623254@std.stei.itb.ac.id>
2025-05-17 00:03:20 +0700

    feat : fix color contrasts

--- frontend/src/app/_components/statistics/Statistics.jsx
+++ frontend/src/app/_components/statistics/Statistics.jsx
@@ -15,15 +15,15 @@ export default function Statistics({ data }) {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center px-4">
       <div className="mb-6 text-center">
-        <h2 className="text-4xl font-Satoshi">{data.commodity}</h2>
-        <p className="text-xl font-Satoshi">Unit: {data.unit}</p>
-        <p className="text-lg font-Satoshi">Recommendation: {data.investment_recommendation}</p>
+        <h2 className="text-4xl font-Satoshi font-bold m-4">{data.commodity}</h2>
+        <p className="text-xl font-Satoshi italic">Unit: {data.unit}</p>
+        <p className="text-lg font-Satoshi font-semibold">Recommendation: {data.investment_recommendation}</p>
       </div>
 
       <ResponsiveContainer width="90%" height="70%">
         <LineChart data={data.prices}>
-          <XAxis dataKey="date" stroke="#a855f7" fontSize={20} interval="preserveStartEnd" />
-          <YAxis stroke="#a855f7" fontSize={20}/>
+          <XAxis dataKey="date" stroke="#a855f7" fontSize={15} interval="preserveStartEnd" tick={{ fill: "#ffffff", fontSize: 15 }} />
+          <YAxis stroke="#a855f7" fontSize={20} tick={{ fill: "#ffffff", fontSize: 15 }}/>
           <CartesianGrid stroke="#4b5563" strokeDasharray="5 5" />
           <Tooltip
             contentStyle={{ fontSize: '15px',  backgroundColor: "#1f1f1f", borderColor: "#a855f7" }} // 👈 Smaller tooltip box text
